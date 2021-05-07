a29a646fcaeb83470fd57dce077aed920ab8b9aa
Sanitaravel <SimCityrulit22@gmail.com>
2021-05-07 15:00:51 +0300

    Initial commit

---
+++ podcasts_browser/layouts/default.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-app light>
+    <v-main>
+      <v-container fill-height>
+        <nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Cairo&display=swap');
+
+.v-application {
+  font-family: 'Cairo', sans-serif;
+}
+
+::-webkit-scrollbar {
+  width: 12px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+  -webkit-border-radius: 10px;
+  border-radius: 10px;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+  -webkit-border-radius: 10px;
+  border-radius: 10px;
+  background: rgba(255, 0, 0, 0.8);
+  -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.5);
+}
+::-webkit-scrollbar-thumb:window-inactive {
+  background: rgba(255, 0, 0, 0.4);
+}
+</style>
---
+++ podcasts_browser/pages/index.vue
@@ -0,0 +1,81 @@
+<template>
+  <v-container fill-height>
+    <v-row justify="center" align="center">
+      <v-col align="center">
+        <h1>Listen Podcasts. Everywhere.</h1>
+        <v-img
+          src="https://faithandleadership.com/sites/default/files/styles/article/public/field/image/listen_podcast_m.png?itok=GG7-tpli"
+        />
+      </v-col>
+      <v-divider vertical></v-divider>
+      <v-col align="center">
+        <v-list flat>
+          <v-list-item fluid>
+            <v-container>
+              <v-form ref="form" v-model="valid" lazy-validation>
+                <v-text-field
+                  v-model="name"
+                  :rules="nameRules"
+                  label="Username"
+                  required
+                  autocomplete="false"
+                ></v-text-field>
+
+                <v-text-field
+                  v-model="password"
+                  label="Password"
+                  :rules="passwordRules"
+                  required
+                  autocomplete="false"
+                  type="password"
+                ></v-text-field>
+
+                <v-container>
+                  <v-btn outlined :disabled="!valid" text @click="validate()">
+                    LOGIN
+                  </v-btn>
+                </v-container>
+              </v-form>
+            </v-container>
+          </v-list-item>
+          <v-divider></v-divider>
+          <v-list-item>
+            <v-container align-content-center>
+              <h2>OR</h2>
+            </v-container>
+          </v-list-item>
+          <v-divider />
+          <v-list-item>
+            <v-container align-content-center>
+              <v-btn text>Register</v-btn>
+            </v-container>
+          </v-list-item>
+        </v-list>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    valid: false,
+    name: '',
+    nameRules: [(v) => !!v || 'Username is required'],
+    password: '',
+    passwordRules: [(v) => !!v || 'Password is required'],
+  }),
+
+  methods: {
+    validate() {
+      this.$refs.form.validate()
+    },
+    reset() {
+      this.$refs.form.reset()
+    },
+    resetValidation() {
+      this.$refs.form.resetValidation()
+    },
+  },
+}
+</script>
